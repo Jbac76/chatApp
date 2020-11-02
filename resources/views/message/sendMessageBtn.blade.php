daccc947fea08c98d85354c2ffebf9098d4ed707
Joe Chavala Jr <jbac76@gmail.com>
2020-11-02 23:24:07 +0300

    upgrade to laravel 8

--- resources/views/message/sendMessageBtn.blade.php
+++
@@ -1,6 +0,0 @@
-
-    @unless( $user->id === auth()->id() )
-        <div>
-            <a href="{{ route('message.create', [$user->id]) }}" class="btn btn-secondary btn-sm pl-2 pr-2 pt-0 pb-0 rounded-0">Send this user a message</a>
-        </div>
-    @endif
